aeb5d3a6e754e6fdc8d900f822ad553e32aaa497
himanshu <himanshusekharnayak1999@gmail.com>
2020-01-02 12:36:04 +0530

    two player can play, with console

--- setup.js
+++ setup.js
@@ -16,16 +16,27 @@ function startGame() {
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 7; j++) {
             origBoard[i][j].innerText = '';
+            origBoard[i][j].style.removeProperty('background-color');
             origBoard[i][j].addEventListener('click', turnClick, false);
         }
     }
+    turnChecker = 0;
 }
 
 //process after clicking a cell
+var turnChecker = 0;
 function turnClick(square) {
-    turn(square.target.id, player1);
+    if(turnChecker%2 == 0){
+        turn(square.target.id, player1);
+        if(!gameWon) console.log(`Player${turnChecker%2 + 1} turn`);
+    }
+    else if(turnChecker%2 == 1){
+        turn(square.target.id, player2);
+        if(!gameWon) console.log(`Player${turnChecker%2 + 1} turn`);
+    }
 }
 
+let gameWon=false;
 function turn(squareId, player) {
 
     let r = parseInt(squareId[0], 10); //r for row
@@ -36,12 +47,22 @@ function turn(squareId, player) {
 
     if (r == 6 || origBoard[r + 1][c].innerText != '') {
         document.getElementById(squareId).innerText = player;
+        turnChecker++;
     }
     else{
         console.log('bad place');
     }
 
-    let gameWon = checkWin(origBoard, player, squareId);
+    gameWon = checkWin(origBoard, player, squareId);
 
-    if(gameWon) console.log('Game Won');
+    if(gameWon){
+        console.log(`${wonPlayer} Won`);
+        for (let i = 0; i < 7; i++) {
+            for (let j = 0; j < 7; j++) {
+                origBoard[i][j].removeEventListener('click', turnClick, false);
+            }
+        }
+    }
 }
+
+startGame();
